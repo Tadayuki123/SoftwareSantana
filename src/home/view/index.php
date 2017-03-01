ec77e6fef6677b27eb2522c0e62a23f3c35289fa
Gabriel Braga <gabriel_o_braga@hotmail.com>
2017-03-01 13:37:44 -0300

    bootstrap

--- src/home/view/index.php
+++ src/home/view/index.php
@@ -1,14 +1,93 @@
-<!DOCTYPE html>
-<html lang="pt">
+<html>
 
 <head>
-    <title>Ótica Santana</title>
-    <meta charset="utf-8">
+    <title> Site Otica Santana</title>
 </head>
-<body>
+<style>
+    form {
+        border: 3px solid #f1f1f1;
+    }
 
-Janela Principal Index
+    button {
+        background-color: #af0103;
+        color: white;
+        padding: 10px 20px;
+        margin: 8px 0;
+        border: 0;
+        cursor: pointer;
+        width: 10%;
+    }
+
+    .imgcontainer {
+        text-align: center;
+        margin: 24px 0 12px 0;
+    }
+
+    .container {
+        padding: 16px;
+    }
+
+    span.psw {
+        float: right;
+        padding-top: 16px;
+    }
+
+    /* Change styles for span and cancel button on extra small screens */
+    @media screen and (max-width: 300px) {
+        span.psw {
+            display: block;
+            float: none;
+        }
+        .cancelbtn {
+            width: 100%;
+        }
+    }
+
+    font {
+        color="#FF0000"
+        size="4"
+        face="comic sans ms"
+
+    }
+</style>
+<body text="#FF0000" link="#FF0000" alink="#FF0000" vlink="#FF0000">
+<p align="center">
+    <br>
+        Otica Santana
+</p>
+
+<p align="center"> Menu:
+
+    <br>
+<table border="0">
+    <tr>
+        <td>    <a href="front.php/index" title="Home"><form><button>Home</button> </form></a>   </td>
+        <td>    <a href="front.php/login" title="Login"><form><button>Login</button></form> </a>    </td>
+        <td>    <a href="front.php/oticaMovel" title="Otica Movel"><form><button>Otica Movel</button></form></a>    </td>
+        <td>    <a href="front.php/contato" title="Contato"><form><button>Contato</button></form></a>    </td>
+    </tr>
+</table>
+<br><br><br>
+</p>
+
+
+<p align="left">
+    Bem vindo a nossa home page!
+
+    Na area de Otica Movel pode se encontrar nosso novo e unico serviço de Otica na porta da sua casa
+
+Na area da galeria pode se encomtrar:
+<blockquote>
+    <ol>
+        <li>Produtos de Marcas exclusivas
+        <li>Armações e Lentes de Marcas Credenciadas
+        <li>Nossas Lojas
+
+</ol>
+
+</blockquote>
+</p>
+
 </body>
 
 </html>
-
